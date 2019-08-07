55201f68b5756a55a73215b18ea4ed3ae315b910
Jaxmano <jordan.prince@mcvts.org>
2019-08-07 16:17:00 -0400

    first command line project

---
+++ MASH.js
@@ -0,0 +1,21 @@
+function mash(hou){
+    
+    return house;
+}
+function getHome(){
+    const housePicked = houses[Math.floor(Math.random()*houses.length)];
+    return housePicked;
+}
+function getChildrenCount(){
+    children = Math.random();
+    children*=100; 
+    let thirdString;
+    return thirdString;
+}
+const house = "house";
+let children;
+let houses = ["Mansion","Apartment","Shack","House"];
+mash();
+console.log(house);
+getHome();
+getChildrenCount();
